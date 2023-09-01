8d0d6625b780cdcf8f9ded65033629a169f76da9
byx2000 <18023699313@163.com>
2023-09-01 17:02:41 +0800

    重构

--- aop-parameter-name/src/main/java/byx/test/Application.java
+++ aop-parameter-name/src/main/java/byx/test/Application.java
@@ -1,8 +1,17 @@
 package byx.test;
 
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
@@ -10,5 +19,29 @@ public class Application {
         ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
         MyController c = ctx.getBean(MyController.class);
         System.out.println(c.method1(1001, "hello"));
+    }
+}
+
+@Aspect
+@Controller
+class MyAspect {
+    @Around("execution(* byx.test.MyController.method1(..))")
+    public Object around(ProceedingJoinPoint pjp) throws Throwable {
+        MethodSignature signature = (MethodSignature) pjp.getSignature();
+        System.out.println("before " + signature.getName());
+        System.out.println("parameters: " + Arrays.toString(signature.getParameterNames()));
+        System.out.println("args: " + Arrays.toString(pjp.getArgs()));
+        Object ret = pjp.proceed();
+        System.out.println("after " + signature.getName());
+        return ret;
+    }
+}
+
+@Component
+@Slf4j
+class MyController {
+    public String method1(Integer id, String msg) {
+        System.out.println("in MyController.method1");
+        return id + " " + msg;
     }
 }
--- sql-init/src/main/java/byx/test/Application.java
+++ sql-init/src/main/java/byx/test/Application.java
@@ -1,5 +1,8 @@
 package byx.test;
 
+import lombok.Data;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -14,4 +17,17 @@ public class Application {
         List<User> users = userMapper.listAllUsers();
         users.forEach(System.out::println);
     }
+}
+
+@Mapper
+interface UserMapper {
+    @Select("SELECT * FROM users")
+    List<User> listAllUsers();
+}
+
+@Data
+class User {
+    private Integer id;
+    private String username;
+    private String password;
 }
